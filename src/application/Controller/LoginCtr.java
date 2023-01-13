fb6a52ade247dcaed16b2ba45e156416f5eee33d
muazabdulr <muaz.abdulr@gmail.com>
2023-01-13 17:40:25 +0700

    tambah validasi login dan close former window

--- src/application/Controller/LoginCtr.java
+++ src/application/Controller/LoginCtr.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
@@ -38,20 +39,30 @@ public class LoginCtr {
 
 	@FXML
 	void LoginButtonPressed(ActionEvent event) {
-		try {
-			Stage primaryStage = new Stage();
-			FXMLLoader xmlloader = new FXMLLoader();
-			xmlloader.setLocation(getClass().getResource("/application/view/MainForm.fxml"));
-			BorderPane root = (BorderPane) xmlloader.load();
-			Scene scene = new Scene(root, 600, 600);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
+		String username = UsernameField.getText();
+		String pass = PassField.getText();
+		if (username.isEmpty() || pass.isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setContentText("username or password can't be empty");
+			alert.showAndWait();
+		} else {
+			try {
+				Stage primaryStage = new Stage();
+				FXMLLoader xmlloader = new FXMLLoader();
+				xmlloader.setLocation(getClass().getResource("/application/view/MainForm.fxml"));
+				BorderPane root = (BorderPane) xmlloader.load();
+				Scene scene = new Scene(root, 600, 600);
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				Stage formerStage = (Stage) LoginButton.getScene().getWindow();
+				formerStage.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
 	}
-	
+
 	@FXML
 	void RegisterButtonPressed(ActionEvent event) {
 		try {
@@ -65,7 +76,7 @@ public class LoginCtr {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
