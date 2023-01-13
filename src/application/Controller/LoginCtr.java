7cdf0a669a69e49e21e2d95c2c512b3002031ffa
muazabdulr <muaz.abdulr@gmail.com>
2023-01-13 15:03:54 +0700

    push app

---
+++ src/application/Controller/LoginCtr.java
@@ -0,0 +1,54 @@
+package application.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class LoginCtr {
+
+	@FXML
+	private Button LoginButton;
+
+	@FXML
+	private PasswordField PassField;
+
+	@FXML
+	private Text PasswordLabel;
+
+	@FXML
+	private Hyperlink RegisterHyperlink;
+
+	@FXML
+	private TextField UsernameField;
+
+	@FXML
+	private Text UsernameLabel;
+
+	@FXML
+	private Text WelcomeLabel;
+
+	@FXML
+	void LoginButtonPressed(ActionEvent event) {
+		try {
+			Stage primaryStage = new Stage();
+			FXMLLoader xmlloader = new FXMLLoader();
+			xmlloader.setLocation(getClass().getResource("/application/view/MainForm.fxml"));
+			BorderPane root = (BorderPane) xmlloader.load();
+			Scene scene = new Scene(root, 600, 600);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/application/Controller/MainFormCtr.java
@@ -0,0 +1,24 @@
+package application.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+public class MainFormCtr {
+
+    @FXML
+    private MenuItem BuyMenuItem;
+
+    @FXML
+    private MenuItem SignOutMenuItem;
+
+    @FXML
+    private MenuItem TransactionHistoryMenuItem;
+
+    @FXML
+    private Menu TransactionMenu;
+
+    @FXML
+    private Menu UserMenu;
+
+}
