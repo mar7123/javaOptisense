69dee6fb845351de6bdf2364c01b699f7a51d925
muazabdulr <muaz.abdulr@gmail.com>
2023-01-16 17:49:12 +0700

    lengkapin order form

--- src/application/Controller/OrderFormCtr.java
+++ src/application/Controller/OrderFormCtr.java
@@ -1,14 +1,24 @@
 package application.Controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import application.DBConnection;
 import application.model.Orders;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class OrderFormCtr {
 
@@ -22,10 +32,10 @@ public class OrderFormCtr {
     private ComboBox<?> newOrderCombo;
 
     @FXML
-    private Spinner<?> newOrderQty;
+    private Spinner<Integer> newOrderQty;
 
     @FXML
-    private Spinner<?> orderQty;
+    private Spinner<Integer> orderQty;
 
     @FXML
     private TableColumn<Orders, String> orderQtyColumn;
@@ -51,8 +61,50 @@ public class OrderFormCtr {
     @FXML
     private TableColumn<Orders, String> vendorColumn;
     
+	ObservableList<Orders> listview = FXCollections.observableArrayList();
+
+    
+    private void loadTableData() {
+    	orderTable.getItems().clear();
+    	orderTable.setRowFactory(rf -> {
+			TableRow<Orders> row = new TableRow<>();
+			row.setOnMouseClicked(event -> {
+				if (!row.isEmpty()) {
+					Orders selected = row.getItem();
+					orderVendor.setText(selected.getVendorName());
+					orderSensor.setText(selected.getSensorName());
+					updateOrderBttn.setVisible(true);
+					cancelOrderBttn.setVisible(true);
+				}
+			});
+			return row;
+		});
+    	vendorColumn.setCellValueFactory(new PropertyValueFactory<>("SensorName"));
+    	productColumn.setCellValueFactory(new PropertyValueFactory<>("SensorPrice"));
+    	orderQtyColumn.setCellValueFactory(new PropertyValueFactory<>("SensorSpeed"));
+    	totalPriceColumn.setCellValueFactory(new PropertyValueFactory<>("SensorStock"));
+		try {
+			Connection c = DBConnection.getKoneksi();
+			PreparedStatement statement = c.prepareStatement("SELECT * FROM orders");
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				listview.add(new Orders(rs.getInt("OrderID"), rs.getInt("SensorID"), rs.getString("ClientCode"),
+						rs.getInt("OrderQuantity")));
+			}
+			orderTable.setItems(listview);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
     void loadInterface() {
-    	
+    	this.loadTableData();
+    	orderVendor.setText("");
+    	orderSensor.setText("");
+    	updateOrderBttn.setVisible(true);
+    	cancelOrderBttn.setVisible(true);
+    	orderQty.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0));
+    	newOrderQty.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0));
     }
 
     @FXML
--- src/application/model/Orders.java
+++ src/application/model/Orders.java
@@ -1,9 +1,17 @@
 package application.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import application.DBConnection;
+
 public class Orders {
 	private int OrderID;
 	private int SensorID;
 	private String ClientCode;
+	private String VendorName;
+	private String SensorName;
 	private int OrderQuantity;
 
 	public Orders(int orderID, int sensorID, String clientCode, int orderQty) {
@@ -11,6 +19,17 @@ public class Orders {
 		this.SensorID = sensorID;
 		this.ClientCode = clientCode;
 		this.OrderQuantity = orderQty;
+		try {
+			Connection c = DBConnection.getKoneksi();
+			PreparedStatement statement = c.prepareStatement("SELECT DISTINCT CompanyName, SensorName FROM companies e inner join orders f on e.CompanyCode = f.ClientCode inner join sensors g on f.SensorID = g.SensorID WHERE SensorID = ? and CompanyCode = ?");
+			statement.setInt(1, this.SensorID);
+			statement.setString(2, this.ClientCode);
+			ResultSet rs = statement.executeQuery();
+			this.VendorName = rs.getString("CompanyName");
+			this.SensorName = rs.getString("SensorName");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public String getOrderID() {
@@ -31,5 +50,13 @@ public class Orders {
 	
 	public int getOrderQuantityInt() {
 		return this.OrderQuantity;
+	}
+	
+	public String getVendorName() {
+		return this.VendorName;
+	}
+	
+	public String getSensorName() {
+		return this.SensorName;
 	}
 }
